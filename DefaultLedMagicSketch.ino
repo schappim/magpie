c274db065bcec0500605c5573b5261ce2f43002f
Marcus Schappi <marcus@schappi.com>
2013-04-19 16:22:00 +1000

    added default led magic sketch

---
+++ DefaultLedMagicSketch.ino
@@ -0,0 +1,49 @@
+/*
+
+Magpie test Sketch.
+LedMagic
+
+ */
+
+void setup() {
+  // declare all ledPin as OUTPUT:
+  DDRD = B11111111;
+  DDRB = DDRB | B00111111;
+  DDRC = DDRC | B00111111;
+}
+
+void loop() {
+  int i=0;
+  for (i=0; i<20; i++)
+  {
+    digitalWrite(i,HIGH);
+    delay(30);
+  }
+ 
+  delay(1000);
+  
+  for (i=0; i<20; i++)
+  {
+    digitalWrite(i,LOW);
+    delay(30);
+  }
+
+  delay(1000);
+  
+  for (i=0; i<20; i++)
+  {
+    digitalWrite(i,HIGH);
+    delay(30);
+    digitalWrite(i,LOW);
+    delay(10);
+  }
+
+  for (i=19; i>-1; i--)
+  {
+    digitalWrite(i,HIGH);
+    delay(30);
+    digitalWrite(i,LOW);
+    delay(10);
+  }
+  
+}
